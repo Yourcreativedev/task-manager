88124e3d1ea83c6d6858de70012b0ecf1e278035
Yourcreativedev <shyamendrahazra@gmail.com>
2022-08-02 16:25:58 +0530

    Added elements into the page

--- index.php
+++ index.php
@@ -10,6 +10,52 @@
     <script src="javascript/script.js"></script>
 </head>
 <body>
-    
+    <nav>
+        <a href="#" class="nav-item">Home</a>
+        <a href="#" class="nav-item">Tasks</a>
+        <a href="#" class="nav-item">Log In</a>
+        <a href="#" class="nav-item">Sign Up</a>
+        <a href="#" class="nav-item">Accounts</a>
+    </nav>
+    <div class="banner">
+        <div class="text">Not Just an Ordinary To-Do List</div>
+        <div class="img"><img src="assets/homepage/checklist image.png" alt="image"></div>
+    </div>
+    <div class="main">
+        <div class="sectons">
+            <div class="text">
+                <ul>
+                    <li>Stay focused on your goals</li>
+                </ul>
+            </div>
+            <div class="divimg">
+                <img src="assets/homepage/Working image.png" alt="focused-image">
+            </div>
+        </div>
+        <div class="sectons">
+            <div class="text">
+                <ul>
+                    <li>Complete your tasks daily</li>
+                </ul>
+            </div>
+            <div class="divimg">
+                <img src="./assets/homepage/Success image.png" alt="success-image">
+            </div>
+        </div>
+        <div class="sectons">
+            <div class="text">
+                <ul>
+                    <li>Find Balance in your schedule</li>
+                </ul>
+            </div>
+            <div class="divimg">
+                <img src="./assets/homepage/balance.png" alt="balance-image">
+            </div>
+        </div>
+    </div>
+    <div class="button">
+        <a href="#">Start Now</a>
+    </div>
+    <div></div>
 </body>
 </html>
