af3d216a2f7c7ca70278f080cfb73a6d00f81c78
Yourcreativedev <shyamendrahazra@gmail.com>
2022-08-03 14:04:04 +0530

    Adding sudden commit  for immediate reasons. Styling of Homepage is undergoing

--- index.php
+++ index.php
@@ -4,58 +4,70 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Task-manager</title>
     <link rel="stylesheet" href="css/styles.css">
     <link rel="stylesheet" href="css/util.css">
     <script src="javascript/script.js"></script>
 </head>
 <body>
-    <nav>
-        <a href="#" class="nav-item">Home</a>
-        <a href="#" class="nav-item">Tasks</a>
-        <a href="#" class="nav-item">Log In</a>
-        <a href="#" class="nav-item">Sign Up</a>
-        <a href="#" class="nav-item">Accounts</a>
-    </nav>
-    <div class="banner">
-        <div class="text">Not Just an Ordinary To-Do List</div>
-        <div class="img"><img src="assets/homepage/checklist image.png" alt="image"></div>
-    </div>
-    <div class="main">
-        <div class="sectons">
-            <div class="text">
-                <ul>
-                    <li>Stay focused on your goals</li>
-                </ul>
-            </div>
-            <div class="divimg">
-                <img src="assets/homepage/Working image.png" alt="focused-image">
-            </div>
+    <div class="container">
+        
+        <!-- Nvabar -->
+        <nav>
+            <a href="#" class="nav-item">Home</a>
+            <a href="#" class="nav-item">Tasks</a>
+            <a href="#" class="nav-item">Log In</a>
+            <a href="#" class="nav-item">Sign Up</a>
+            <a href="#" class="nav-item">Accounts</a>
+        </nav>
+        <!-- End Of Nav -->
+
+        <!-- Banner -->
+        <div class="banner">
+            <h1 class="text">Not Just an Ordinary<br /> <span>To-Do</span> List</h1>
+            <div class="img"><img src="assets/homepage/checklist image.png" alt="image"></div>
         </div>
-        <div class="sectons">
-            <div class="text">
-                <ul>
-                    <li>Complete your tasks daily</li>
-                </ul>
-            </div>
-            <div class="divimg">
-                <img src="./assets/homepage/Success image.png" alt="success-image">
-            </div>
+        <!-- End Of Banner -->
+
+
+        <!-- Main section -->
+        <div class="main">
+            
+        
+            <ul>
+                <li>Stay focused on your goals</li>
+            </ul>    
+            <img src="assets/homepage/Working image.png" alt="focused-image">
+                
+            <!-- End Of First Section -->
+
+    
+            <ul>
+                <li>Complete your tasks daily</li>
+            </ul>
+            <img src="./assets/homepage/Success image.png" alt="success-image">
+                
+            <!-- End Of Second Section -->
+
+    
+            <ul>
+                <li>Find Balance in your schedule</li>
+            </ul>
+            <img src="./assets/homepage/balance.png" alt="balance-image">
+                
+            <!-- End Of Third Section -->
+
+
         </div>
-        <div class="sectons">
-            <div class="text">
-                <ul>
-                    <li>Find Balance in your schedule</li>
-                </ul>
-            </div>
-            <div class="divimg">
-                <img src="./assets/homepage/balance.png" alt="balance-image">
-            </div>
+        <!-- End Of Main Section -->
+
+        <!-- Button -->
+        <div class="button">
+            <a href="#">Start Now</a>
         </div>
+        <!-- End Of Button -->
+
     </div>
-    <div class="button">
-        <a href="#">Start Now</a>
-    </div>
-    <div></div>
+    <!-- End Of Container -->
 </body>
 </html>
